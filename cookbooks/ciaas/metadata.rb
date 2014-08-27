89337defa832a345c45d42aeedf382f4ac3cda7f
Pavel Egorov <egorovpasha@gmail.com>
2014-08-27 19:49:30 +0100

    metadata fixed

--- cookbooks/ciaas/metadata.rb
+++ cookbooks/ciaas/metadata.rb
@@ -5,3 +5,6 @@ license          ''
 description      'Installs/Configures ciaas'
 long_description 'Installs/Configures ciaas'
 version          '0.1.0'
+
+depends 'mysql'
+depends 'redisio'
--- roles/dev.rb
+++ roles/dev.rb
@@ -3,10 +3,11 @@ name "dev"
 
 # Run list function we mentioned earlier
 run_list(
+	"recipe[runit]",
     "recipe[nodejs]",
     "recipe[docker]",
     "recipe[mysql::server]",
-    "recipe[redisio::install]",
+    "recipe[redisio]",
     "recipe[redisio::enable]",
     "recipe[ciaas]",
 )
