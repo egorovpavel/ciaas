6bb7d08c04d542da3ea553a8373bcd5f15ff7056
Pavel Egorov <egorovpasha@gmail.com>
2014-09-07 17:35:51 +0100

    migrateup recipe updated

--- cookbooks/aws-ciaas/recipes/migrateup.rb
+++ cookbooks/aws-ciaas/recipes/migrateup.rb
@@ -6,7 +6,15 @@
 #
 # 
 #
+log "message" do
+  message "Executing migrateup"
+  level :info
+end
 execute "npm-migrateUp" do
 	cwd '/srv/www/ciaas_web/current'
     command 'npm run-script migrateUp'
+end
+log "message" do
+  message "Finishing executing migrateup"
+  level :info
 end
