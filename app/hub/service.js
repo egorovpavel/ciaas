d178b5b2aafc356077019db063dd272f8e7aaa5e
Pavel Egorov <egorovpasha@gmail.com>
2014-09-10 03:12:50 +0100

    fixed hub entry point script

--- app/hub/service.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-var Hub = require('./lib/hub.js');
-var Logger = require('winston');
-var persistanceHandler = require('./lib/SQLPersistanceHandler.js');
-var config = require('./config.json')[process.env.NODE_ENV || 'development'];
-var hub = new Hub(config.redis, new persistanceHandler(config.mysql),null,Logger);
-hub.handle();
